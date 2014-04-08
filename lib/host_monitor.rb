76b637e1a486dab778ed1bf76d8d1173d21f56c4
ilya_scherbinin <m0n9oose@gmail.com>
2014-04-09 00:09:47 +0400

    fix error code detection

--- lib/host_monitor.rb
+++ lib/host_monitor.rb
@@ -9,7 +9,7 @@ class HostMonitor
 
   def start
     host.ping
-    if host.match(/5\d\d|404/)
+    if host.code.match(/5\d\d|404/)
       notify(:down, $config['notification'], host.details) if [3, 10, 50, 100, 500].include? @tries
       @tries += 1
       sleep $config['host']['timeout'].to_i
